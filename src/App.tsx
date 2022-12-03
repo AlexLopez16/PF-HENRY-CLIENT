fda22d2fcd1caf17eb49517453e4444c1ce7b0b1
Amparo-E <escobarampii@gmail.com>
2022-12-03 04:33:49 -0300

    LandingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,41 @@
-import { Provider } from "react-redux"
-import { AppRouter } from "./routers/AppRouter"
-import { store } from "./store/store"
+import { Provider } from "react-redux";
+import { AppRouter } from "./routers/AppRouter";
+import { store } from "./store/store";
+import { ThemeProvider, CssBaseline, createTheme} from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      // '-apple-system',
+      // 'BlinkMacSystemFont',
+      // '"Segoe UI"',
+      'Avenir Medium',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+  },
+  // palette: {
+  //   'mode': 'dark',
+  //   // 'background': {
+  //   //   'default': '#000000bf'
+  //   // }
+  // }
+})
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="App">
-        <AppRouter />
-      </div>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+        <Provider store={store}>
+          <div className="App">
+            <AppRouter />
+          </div>
+        </Provider>
+    </ThemeProvider>
   )
 }
 
