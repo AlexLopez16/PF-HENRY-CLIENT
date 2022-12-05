d47fb7b96fc6447a448034aa2d57ac6945a15840
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2022-12-05 11:20:41 -0300

    companyForm added

--- src/components/Forms/UserForm.tsx
+++ src/components/Forms/UserForm.tsx
@@ -1,62 +1,22 @@
-import React, { useState } from "react";
-import { FC } from "react";
-import { Grid, Button, Box, Paper, FormHelperText } from "@mui/material";
+import { useState, FC } from "react";
+import {
+  Grid,
+  Button,
+  Paper,
+  FormHelperText,
+  Divider,
+  TextField,
+  InputLabel,
+  FormControl,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as yup from "yup";
-import { FieldProps, getIn } from "formik";
-
-import { TextFieldProps, TextField } from "@mui/material";
-
-interface FormValues {
-  name: string;
-  apellido: string;
-  email: string;
-  password: string;
-  passwordRepeat: string;
-  showPassword:boolean;
-}
-
-// the Formik component supports yup validation out-of-the-box via the `validationSchema` prop
-const validationSchema = yup.object().shape({
-  name: yup.string().required("Nombre requerido"),
-  apellido: yup.string().required("Apellido requerido"),
-  email: yup.string().email().required("Email requerido"),
-  password: yup
-    .string()
-    .min(8, 'Debe contener min. 8 caracter')
-    .matches(/[0-9]/, 'Se requiere un numero')
-    .matches(/[a-z]/, 'Se requiere una letra minuscula')
-    .matches(/[A-Z]/, 'Se requiere una letra mayuscula')
-    .matches(/[^\w]/, 'Se requiere un simbolo'),
-
-    passwordRepeat:yup
-          .string()
-          .oneOf([yup.ref('password'), null], 'No coinciden las contraseñas'),
-});
-
-/**
- * Material TextField Component with Formik Support including Errors.
- * Intended to be specified via the `component` prop in a Formik <Field> or <FastField> component.
- * Material-UI specific props are passed through.
- */
-export const FormTextField: React.FC<FieldProps & TextFieldProps> = (props) => {
-  const isTouched = getIn(props.form.touched, props.field.name);
-  const errorMessage = getIn(props.form.errors, props.field.name);
-
-  const { error, helperText, field, form, ...rest } = props;
-
-  return (
-    <TextField
-      variant="outlined"
-      error={error ?? Boolean(isTouched && errorMessage)}
-      helperText={
-        helperText ?? (isTouched && errorMessage ? errorMessage : undefined)
-      }
-      {...rest}
-      {...field}
-    />
-  );
-};
+import { VisibilityOff, Visibility, Message } from "@mui/icons-material";
+import { GitHubLogin } from "../auth/GitHubLogin";
+import { GoogleLogin } from "../auth/GoogleLogin";
 
 export const UserForm: FC = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -92,46 +52,40 @@ export const UserForm: FC = () => {
         <Paper
           elevation={10}
           style={{
-            width: 400,
+            width: 380,
             height: "100%",
             padding: 20,
             margin: "50px auto",
           }}
         >
-          login de alumnos <br />
-          como viene el front papa!!!!!!!!!!!!!!
-        </Typography>
-        <Typography align="center">grupo 5</Typography>
-      </Box>
-      <Formik
-        initialValues={{
-          name: "",
-          apellido: "",
-          email: "",
-          password: "",
-          passwordRepeat: "",
-          showPassword:false,
-        }}
-        validationSchema={validationSchema}
-        onSubmit={(
-          values: FormValues,
-          formikHelpers: FormikHelpers<FormValues>
-        ) => {
-          alert(JSON.stringify(values, null, 2));
-          formikHelpers.setSubmitting(false);
-        }}
-      >
-        {(formikProps: FormikProps<FormValues>) => (
-          <Form noValidate autoComplete="off">
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
+          <Grid textAlign="center">
+            <h2>Crear cuenta</h2>
+          </Grid>
+          <GitHubLogin />
+          <GoogleLogin />
+          <Divider>
+            <span>O</span>
+          </Divider>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={onSubmit}
+          >
+            {(props) => (
+              <Form>
                 <Field
                   as={TextField}
                   name="name"
                   label="Nombre"
                   size="small"
-                  sx={{ width: "100%" }}
-                  helperText={<ErrorMessage name="name" />}
+                  sx={{ width: "100%", margin: "10px 0" }}
+                  helperText={
+                    <ErrorMessage name="name">
+                      {(message) => (
+                        <span style={{ color: "red" }}>{message}</span>
+                      )}
+                    </ErrorMessage>
+                  }
                 />
 
                 <Field
@@ -139,8 +93,14 @@ export const UserForm: FC = () => {
                   name="apellido"
                   label="Apellido"
                   size="small"
-                  sx={{ width: "100%" }}
-                  helperText={<ErrorMessage name="apellido" />}
+                  sx={{ width: "100%", margin: "10px 0" }}
+                  helperText={
+                    <ErrorMessage name="apellido">
+                      {(message) => (
+                        <span style={{ color: "red" }}>{message}</span>
+                      )}
+                    </ErrorMessage>
+                  }
                 />
 
                 <Field
@@ -148,10 +108,16 @@ export const UserForm: FC = () => {
                   name="email"
                   label="Email"
                   size="small"
-                  sx={{ width: "100%" }}
-                  helperText={<ErrorMessage name="email" />}
+                  sx={{ width: "100%", margin: "10px 0" }}
+                  helperText={
+                    <ErrorMessage name="email">
+                      {(message) => (
+                        <span style={{ color: "red" }}>{message}</span>
+                      )}
+                    </ErrorMessage>
+                  }
                 />
-                <FormControl sx={{ width: "100%" }}>
+                <FormControl sx={{ width: "100%", margin: "10px 0" }}>
                   <InputLabel htmlFor="contraseña">Contraseña</InputLabel>
                   <Field
                     as={OutlinedInput}
