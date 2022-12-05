c03aafee8976b327531f208f293f4c9b452401e9
AlexLopez16 <aalejandro.lopezr@hotmail.com>
2022-12-05 10:28:27 -0600

    Cambios

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <LoginScreen />
+        {/* <LoginScreen /> */}
         {/* <AppRouter /> */}
         {/* <AppRouter /> */}
 
