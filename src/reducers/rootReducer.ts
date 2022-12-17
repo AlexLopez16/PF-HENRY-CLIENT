2dca62bf9ffb3e0f1c9a257a6828e15a5211d374
sciangulahugo <110142598+sciangulahugo@users.noreply.github.com>
2022-12-17 12:34:51 -0300

    Show Errors BACKEND

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,17 +1,17 @@
-import { combineReducers } from 'redux'
-import { authReducer } from './authReducer'
+import { combineReducers } from 'redux';
+import { authReducer } from './authReducer';
 import { studentReducer } from './studentReducer';
-
 
 import { companyReducer } from './companyReducer';
 import { projectReducer } from './ProjectReducer';
-
+import { errorReducer } from './errorReducer';
 
 export const rootReducer = combineReducers({
     auth: authReducer,
     student: studentReducer,
     project: projectReducer,
     company: companyReducer,
-})
+    error: errorReducer,
+});
 
-export type State = ReturnType<typeof rootReducer>
+export type State = ReturnType<typeof rootReducer>;
