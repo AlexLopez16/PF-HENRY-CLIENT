35fc8fa38bb5a52e48a1abbec1de855634c0838f
joalega <jalexisgarcia.91@gmail.com>
2022-12-21 12:28:48 -0300

    correcciones

--- src/pages/PageRegister.tsx
+++ src/pages/PageRegister.tsx
@@ -1,65 +1,89 @@
-import { FC } from "react";
-import { Button, Typography, Box } from "@mui/material";
-import { Link } from "react-router-dom";
-import style from "../styles/register.module.css";
+import { FC } from 'react';
+import { Button, Typography, Box, Grid } from '@mui/material';
+import { Link } from 'react-router-dom';
+import style from '../styles/register.module.css';
+import registerbg from '../../public/assets/registerbg.png';
 
 const buttonSignIn = {
-  alignItems: "center",
-  background: "#272727",
+  alignItems: 'center',
+  background: '#272727',
   border: 2,
   borderRadius: 8,
-  color: "#ffd700",
-  cursor: "pointer",
+  color: '#ffd700',
+  cursor: 'pointer',
   fontSize: 16,
   height: 48,
-  justifyContent: "center",
-  maxWidth: "100%",
-  p: "0 25px",
-  position: "relative",
-  userSelect: "none",
+  justifyContent: 'center',
+  maxWidth: '100%',
+  p: '0 25px',
+  position: 'relative',
+  userSelect: 'none',
   fontWeight: 700,
-  "&:hover": {
-    background: "#ffd700",
-    border: "none",
-    color: "#272727",
+  '&:hover': {
+    background: '#ffd700',
+    border: 'none',
+    color: '#272727',
     TransitionEvent: 1,
   },
 };
 export const Register: FC = () => {
   return (
     <>
-      <div className={style.content_top}>
-        <Typography textAlign="center" variant="h4">
-        Como empresa crea un proyecto o como alumno se parte de uno. 
+      <Box
+        height='100vh'
+        display='flex'
+        justifyContent='space-around'
+        sx={{
+          backgroundImage: `url(${registerbg})`,
+        }}
+      >
+        {/* <div className={style.card_transition}>
+        <Typography textAlign='center' variant='h4' color='primary'>
+          Como empresa crea un proyecto o como alumno se parte de uno.
         </Typography>
       </div>
-      <Box
-        height="100vh"
-        display="flex"
-        justifyContent="space-around"
-        sx={{ background: "#e8ca68" }}
-      >
         <div className={style.content_left}>
-          <Link className={style.link} to="/signup/student">
-            <Button sx={buttonSignIn} variant="outlined">
+          <Link className={style.link} to='/signup/student'>
+            <Button sx={buttonSignIn} variant='outlined'>
               Estudiante
             </Button>
           </Link>
         </div>
         <div className={style.content_right}>
-          <Link className={style.link} to="/signup/company">
-            <Button sx={buttonSignIn} variant="outlined">
+          <Link className={style.link} to='/signup/company'>
+            <Button sx={buttonSignIn} variant='outlined'>
               Empresa
             </Button>
           </Link>
-          
-        </div>
-{/*         
-          <div className={style.content_button}>
-        <Typography textAlign="center" variant="h4">
-        Como empresa crea un proyecto o como alumno se parte de uno. 
-        </Typography>
         </div> */}
+      
+        <Grid
+          container
+          direction='row'
+          justifyContent='center'
+          alignItems='center'
+          textAlign='center'
+        >
+          <Grid item xs={4}>
+            <div className={style.content_left}>
+              <Link className={style.link} to='/signup/student'>
+                <Button sx={buttonSignIn} variant='outlined'>
+                  Estudiante
+                </Button>
+              </Link>
+            </div>
+          </Grid>
+          <Grid item xs={3}>
+            <div className={style.content_right}>
+              <Link className={style.link} to='/signup/company'>
+                <Button sx={buttonSignIn} variant='outlined' color='secondary'>
+                  Empresa
+                </Button>
+              </Link>
+            </div>
+          </Grid>
+        </Grid>
+        
       </Box>
     </>
   );
