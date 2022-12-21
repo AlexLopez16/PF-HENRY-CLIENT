919ddd7e1d27ce2f6b62ece2ae691a235d9a4fbb
Ignacio <ignaciosanchezgirardssj@gmail.com>
2022-12-21 17:47:48 -0300

    Menu de usuario

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,9 +1,15 @@
 import { FC } from 'react';
 import DashboardStudens from '../components/student/DashboardStudent';
 import DashboardCompany from '../components/company/DashboardCompany';
+import { useSelector } from 'react-redux';
+import { State } from '../reducers/rootReducer';
 
 const DashboardPage: FC = () => {
-    const rol = localStorage.getItem('rol');
+    // const rol = localStorage.getItem('rol');
+
+    const { rol } = useSelector((state: State) => state.auth.data);
+
+    let role = rol
 
     return rol === 'STUDENT_ROL' ? <DashboardStudens /> : <DashboardCompany />;
     //role === 'Student' ? <DashStudent/> : <DashCompany/>
