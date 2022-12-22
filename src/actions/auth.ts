4afca57d721a5a8c53530e267900b36815ac3f93
sciangulahugo <110142598+sciangulahugo@users.noreply.github.com>
2022-12-22 01:14:47 -0300

    secure front

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -1,62 +1,100 @@
-import { Dispatch } from 'redux'
-import axios from 'axios'
+import { Dispatch } from 'redux';
+import axios from 'axios';
 import { types } from '../types/types';
 
+/**
+ * By Sciangula Hugo.
+ * NOTA: con infoToken() si tenemos un token valido en el local storage, vamos a mantener la sesion con ese token.
+ */
+
+export const validaToken = (token: any) => {
+    console.log(token);
+    return async (dispatch: Dispatch) => {
+        try {
+            const { data, status } = await axios.get(
+                'http://localhost:3001/api/token',
+                {
+                    headers: { 'user-token': token },
+                }
+            );
+            const { id, rol } = data;
+            if (status) {
+                localStorage.setItem('token', token);
+                dispatch(login({ data, status, id, rol }));
+            }
+        } catch (error: any) {
+            console.log(error);
+            dispatch(logout());
+            // dispatch({
+            //     type: types.authLogin,
+            //     logged: false,
+            //     payload: {
+            //         status: error.response.status,
+            //     },
+            // });
+        }
+    };
+};
 
 export const startLogin = (values: object) => {
     return async (dispatch: Dispatch) => {
         try {
-            const { data, status } = await axios.post('/auth', values)
-            const { token,id,rol} = data;
+            const { data, status } = await axios.post('/auth', values);
+            const { token, id, rol } = data;
             if (status) {
                 localStorage.setItem('token', token);
                 localStorage.setItem('id', id);
                 localStorage.setItem('rol', rol);
-                dispatch(login({ data, status,id,rol }))
+                dispatch(login({ data, status, id, rol }));
             }
         } catch (error: any) {
             dispatch({
                 type: types.authLogin,
                 logged: false,
                 payload: {
-                    status: error.response.status
-                }
-            })
+                    status: error.response.status,
+                },
+            });
         }
-    }
-}
-export const githubLogin = ({id,rol,token}) => {
-            return login({data:{id,rol,token}})         
-}
+    };
+};
+export const githubLogin = ({ id, rol, token }) => {
+    return login({ data: { id, rol, token } });
+};
 
-export const gmailLogin=(tok:String,userType:String)=>{
+export const gmailLogin = (tok: String, userType: String) => {
     return async (dispatch: Dispatch) => {
-      
         try {
-            const { data, status } = await axios.post('/auth', {from:'gmail',tok,userType})
-            const { token,id,rol } = data;
-    
+            const { data, status } = await axios.post('/auth', {
+                from: 'gmail',
+                tok,
+                userType,
+            });
+            const { token, id, rol } = data;
+
             if (status) {
                 localStorage.setItem('token', token);
                 localStorage.setItem('id', id);
                 localStorage.setItem('rol', rol);
-                dispatch(login({ data,status,token,id,rol  }))
+                dispatch(login({ data, status, token, id, rol }));
             }
         } catch (error: any) {
             dispatch({
                 type: types.authLogin,
                 logged: false,
                 payload: {
-                    status: error.response.status
-                }
-            })
+                    status: error.response.status,
+                },
+            });
         }
-    }
-
-}
+    };
+};
 
 const login = (data: object) => ({
     type: types.authLogin,
-    payload: data
-})
+    payload: data,
+});
 
+const logout = () => ({
+    type: types.clearAuthLogin,
+});
