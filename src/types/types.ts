9f574c8c02561c5f8d50b0ae0c39337095c7301e
ignacio-nicoletti <nicolettiignacio5@gmail.com>
2022-12-28 15:13:05 -0300

    types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,41 +1,19 @@
 export const types = {
-    authLogin: '[auth] Login User',
-    authLoginGit: '[auth] GitHub Login',
-    clearAuthLogin: '[auth] Clear Auth Login',
+  authLogin: '[auth] Login User',
+  authLoginGit: '[auth] GitHub Login',
+  clearAuthLogin: '[auth] Clear Auth Login',
 
-    studentRegister: '[student] Register Student',
-    studentGetInfo: '[student] Get Info Student',
-    studentUpdateInfo: '[student] Update Info Student',
-    studentSearch: '[student] Search Company', //-VERIFICAR-//
-    AddStToPr: '[student] Add student to project',
+  studentRegister: '[student] Register Student',
+  studentGetInfo: '[student] Get Info Student',
+  studentUpdateInfo: '[student] Update Info Student',
+  studentSearch: '[student] Search Company', //-VERIFICAR-//
+  AddStToPr: '[student] Add student to project',
 
-    registerCompany: '[company] Register Company',
-    companyGetInfo: '[company] Get info company',
-    companyUpdateInfo: '[company] Update info company',
-    companySearch: '[company] Serch Company', //-VERIFICAR-//
-    acceptStudent: '[Company] Student Accept',
-
-    newProject: '[project] Create New Project',
-    getProjects: '[project] Get Projects',
-    projectsFilter: '[projects], get filtered projects',
-    getProjectById: '[project] Get Project By Id',
-    getCategory: '[project] Get Category',
-    getMyProjectCompany: '[project]  Get My Project Company',
-    filters: '[project] Filters',
-
-    showError: '[error] Show Error',
-    clearError: '[error] Clear Error',
-  studentRegister: "[student] Register Student",
-  studentGetInfo: "[student] Get Info Student",
-  studentUpdateInfo: "[student] Update Info Student",
-  studentSearch: "[student] Search Company", //-VERIFICAR-//
-  AddStToPr: "[student] Add student to project",
-
-  registerCompany: "[company] Register Company",
-  companyGetInfo: "[company] Get info company",
-  companyUpdateInfo: "[company] Update info company",
-  companySearch: "[company] Serch Company", //-VERIFICAR-//
-  acceptStudent: "[Company] Student Accept",
+  registerCompany: '[company] Register Company',
+  companyGetInfo: '[company] Get info company',
+  companyUpdateInfo: '[company] Update info company',
+  companySearch: '[company] Serch Company', //-VERIFICAR-//
+  acceptStudent: '[Company] Student Accept',
   deleteStudent: "[Company] Student delete",
 
   newProject: '[project] Create New Project',
@@ -44,6 +22,7 @@ export const types = {
   getProjectById: '[project] Get Project By Id',
   getCategory: '[project] Get Category',
   getMyProjectCompany: '[project]  Get My Project Company',
+  filters: '[project] Filters',
 
   showError: '[error] Show Error',
   clearError: '[error] Clear Error',
