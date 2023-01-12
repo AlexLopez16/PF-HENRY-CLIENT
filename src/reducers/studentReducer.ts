e34de1c94281ade88745f46433b0e324d971c6c6
Silvana-Murillas <110003502+Silvana-Murillas@users.noreply.github.com>
2023-01-11 23:20:26 -0500

    correccion de bug en paginado studen admin

--- src/reducers/studentReducer.ts
+++ src/reducers/studentReducer.ts
@@ -1,88 +1,94 @@
 // import { type, userInfo } from "os";
-import { types } from "../types/types";
+import { types } from '../types/types';
 
 interface State {
-  users: {}[];
-  user: object | any;
-  total1: number | any;
+    users: {}[];
+    user: object | any;
+    total1: number | any;
 }
 
 const initialState: object | any = {
-  users: [],
-  user: {
-    project: [],
-  },
-  total1: 0,
+    users: [],
+    user: {
+        project: [],
+    },
+    total1: 0,
 };
 
 type Action = {
-  type: string;
-  payload?: Object | any;
+    type: string;
+    payload?: Object | any;
 };
 
 export const studentReducer = (state: State = initialState, action: Action) => {
-  switch (action.type) {
-    case types.getListStudents:
-      const { total, students }: any = action.payload;
-      return {
-        ...state,
-        users: students,
-        total1: total,
-      };
+    switch (action.type) {
+        case types.getListStudents:
+            const { total, students }: any = action.payload;
+            return {
+                ...state,
+                users: students,
+                total1: total,
+            };
 
-    case types.studentRegister:
-      return {
-        ...state,
-        user: action.payload,
-      };
+        case types.studentRegister:
+            return {
+                ...state,
+                user: action.payload,
+            };
 
-    case types.studentGetInfo:
-      return {
-        ...state,
-        user: action.payload,
-      };
+        case types.studentGetInfo:
+            return {
+                ...state,
+                user: action.payload,
+            };
 
-    case types.studentUpdateInfo:
-      return {
-        ...state,
-        user: { ...state.user, ...action.payload },
-      };
+        case types.studentUpdateInfo:
+            return {
+                ...state,
+                user: { ...state.user, ...action.payload },
+            };
 
-    case types.studentSearch:
-      return {
-        ...state,
-        user: action.payload,
-      };
-    case types.deleteOrInactiveStudent:
-      let users: any[] = state.users;
-      for (let index = 0; index < users?.length; index++) {
-        let currentValue: any = users[index];
+        case types.studentSearch:
+            return {
+                ...state,
+                user: action.payload,
+            };
+        case types.deleteOrInactiveStudent:
+            let users: any[] = state.users;
+            for (let index = 0; index < users?.length; index++) {
+                let currentValue: any = users[index];
 
-        if (currentValue.uid === action?.payload?.uid) {
-          currentValue = action.payload;
-          users[index] = currentValue;
-        }
-      }
-      return {
-        ...state,
-        user: { ...state.user.project, project: users },
-      };
-    case types.multipleSwitchAlumno:
-      return {
-        ...state,
-      };
-    case types.unApplyStudent:
-      const newUser: object | any = state.user;
-      const filterProjects: object | any = state.user.project.filter(
-        (e: object | any) => e.uid != action.payload
-      );
-      newUser.project = filterProjects;
-      newUser.working = [];
-      return {
-        ...state,
-        user: newUser,
-      };
-    default:
-      return state;
-  }
+                if (currentValue.uid === action?.payload?.uid) {
+                    currentValue = action.payload;
+                    users[index] = currentValue;
+                }
+            }
+            return {
+                ...state,
+                user: { ...state.user.project, project: users },
+            };
+        case types.multipleSwitchAlumno:
+            return {
+                ...state,
+            };
+        case types.unApplyStudent:
+            const newUser: object | any = state.user;
+            const filterProjects: object | any = state.user.project.filter(
+                (e: object | any) => e.uid != action.payload
+            );
+            newUser.project = filterProjects;
+            newUser.working = [];
+            return {
+                ...state,
+                user: newUser,
+            };
+        case types.clearStudentAdmin:
+            return {
+                ...state,
+                users: action.payload.users,
+                total1: action.payload.total,
+            };
+        default:
+            return state;
+    }
 };
